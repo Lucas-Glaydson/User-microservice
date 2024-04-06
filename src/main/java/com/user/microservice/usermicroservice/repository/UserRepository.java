5649fc29f3be7c96c9959d2da9615c17fe349d74
lucas <lucas.glaydson01@hotmail.com>
2024-04-06 12:59:31 -0300

    fix: crate find user by email returning UserDetails

--- src/main/java/com/user/microservice/usermicroservice/repository/UserRepository.java
+++ src/main/java/com/user/microservice/usermicroservice/repository/UserRepository.java
@@ -2,7 +2,10 @@ package com.user.microservice.usermicroservice.repository;
 
 import com.user.microservice.usermicroservice.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserModel, String> {
-
+    UserDetails findByEmail(String email);
 }
